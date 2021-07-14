33e7dec3306fb27f74374000670ae50d5bc4e59e
Taekhun <nth9708@naver.com>
2021-07-14 22:07:14 +0900

    프로그래머스 : 네트워크

---
+++ JavaScript/DFS,BFS/네트워크/main.js
@@ -0,0 +1,31 @@
+function solution(n, computers) {
+  var answer = 0;
+
+  const check = [];
+
+  function DFS(index) {
+    check[index] = true;
+    for (let i = 0; i < computers.length; i++) {
+      if (computers[index][i] === 1 && !check[i]) {
+        DFS(i);
+      }
+    }
+  }
+
+  for (let i = 0; i < computers.length; i++) {
+    if (!check[i]) {
+      DFS(i);
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(3, [
+    [1, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+  ])
+);
