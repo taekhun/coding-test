b2773f2f0395152657c5e39be8984e22d0bd90ec
Taekhun <nth9708@naver.com>
2021-07-20 22:49:10 +0900

    프로그래머스 : 해시 - 베스트앨범(refactor)

--- JavaScript/해시/베스트앨범.js
+++ JavaScript/해시/베스트앨범.js
@@ -1,28 +1,15 @@
 function solution(genres, plays) {
   let answer = [];
   let obj = {};
-
-  plays.forEach((v, idx) => {
-    obj[genres[idx]] = (obj[genres[idx]] || 0) + v;
-  });
-
+  plays.forEach((v, idx) => (obj[genres[idx]] = (obj[genres[idx]] || 0) + v));
   const sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
-
   obj = {};
 
   sorted.forEach((s) => {
     let arr = [];
-
-    genres.forEach((v, idx) => {
-      v === s[0] && arr.push([plays[idx], idx]);
-    });
-
+    genres.forEach((v, idx) => v === s[0] && arr.push([plays[idx], idx]));
     arr = arr.sort((a, b) => b[0] - a[0]);
-    arr
-      .filter((v, idx) => idx < 2)
-      .forEach((v) => {
-        answer.push(v[1]);
-      });
+    arr.filter((v, idx) => idx < 2).forEach((v) => answer.push(v[1]));
   });
 
   return answer;
