cc2177ad4fa087e0cedf120fc85cbf4faf158538
Taekhun <nth9708@naver.com>
2022-02-06 20:19:19 +0900

    큐: 다리를 지나는 트럭(수정)

--- 스택,큐/다리를 지나는 트럭.js
+++ 스택,큐/다리를 지나는 트럭.js
@@ -1,14 +1,24 @@
 function solution(bridge_length, weight, truck_weights) {
   let count = 0;
-  let truck_stack = [];
+  let bridge_queue = new Array(bridge_length).fill(0);
 
   while (truck_weights.length) {
     for (let i = 0; i < bridge_length; i++, count++) {
-      if (sum(truck_stack) + truck_weights[0] <= weight) {
-        truck_stack.push(truck_weights.shift());
+      bridge_queue.shift();
+
+      if (sum(bridge_queue) + truck_weights[0] <= weight) {
+        bridge_queue.push(truck_weights.shift());
+      } else {
+        bridge_queue.push(0);
       }
     }
-    truck_stack.shift();
+
+    if (truck_weights.length === 0) {
+      while (sum(bridge_queue) !== 0) {
+        count += 1;
+        bridge_queue.shift();
+      }
+    }
   }
 
   return count;
@@ -18,6 +28,7 @@ function sum(arr) {
   return arr.reduce((a, b) => a + b, 0);
 }
 
-console.log(solution(2, 10, [7, 4, 5, 6]));
-console.log(solution(100, 100, [10]));
-console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
+// console.log(solution(2, 10, [7, 4, 5, 6]));
+// console.log(solution(100, 100, [10]));
+// console.log(solution(10, 10, [10]));
+// console.log(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]));
